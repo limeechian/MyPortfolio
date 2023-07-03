3fdf392609dcb4517d94fd1de54116bf25bb26a2
limeechian <limeechian0211@gmail.com>
2023-07-03 19:41:41 +0800

    put java git hub link, change CV

--- js/script.js
+++ js/script.js
@@ -2,14 +2,12 @@
 
 /* The code block below is written in JavaScript and utilizes jQuery.
 It sets up an event handler for a click event on an element with the id `menu` ('#menu')
-and perform certain actions when the element is clicked. 
+and perform certain actions when the element is clicked. */
 
-
-*/
 $(document).ready(function() {  /* This code block sets up an event handler that triggers(is executed) when the document is fully loaded and ready.
                                    It ensures that the code inside the function is executed only when the DOM(Document Object Model) is ready for manipulation. */
 
-    $('#menu').click(function() {  /* This code sets up an event handler(is executed) for the element with the id `menu` that is clicked.
+    $('#mnu').click(function() {  /* This code sets up an event handler(is executed) for the element with the id `menu` that is clicked.
                                       It targets the element and attaches a function that will be executed when the element is clicked. */
         $(this).toggleClass('fa-times');  /* Inside the click event handler, this line toggles the presence of the CSS class 'fa-times' on the clicked element(`$(this)`). 
                                              If the class 'fa-times' is already present, the `toggleClass()` function will be removed. If the class is not present, the function will be added. */
@@ -43,6 +41,6 @@ $(document).ready(function() {  /* This code block sets up an event handler that
             'linear'
         );
     });
-
+    
 });
 
